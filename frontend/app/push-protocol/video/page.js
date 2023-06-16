ba6aca2098f9f7ebb17dcec18129232df5742308
Aditya Bhattad <adityabhattad18@gmail.com>
2023-06-16 11:50:11 +0530

    Init Video Section

--- frontend/app/push-protocol/video/page.js
+++ frontend/app/push-protocol/video/page.js
@@ -1,7 +1,16 @@
+"use client";
+import { Chat } from "@pushprotocol/uiweb";
+import { ITheme } from "@pushprotocol/uiweb";
+import { useSearchParams } from "next/navigation";
+import { useAccount } from "wagmi";
+
 export default function Video() {
+  const {address}=useAccount()
+  const searchParams = useSearchParams();
+  const creatorAddress = searchParams.get("creatorAddress");
   return (
     <div>
-      <h1>Video</h1>
+     
     </div>
   );
 }
